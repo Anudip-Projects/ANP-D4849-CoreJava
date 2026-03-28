a7a96f74da46041b904eef5db45a9eccfebaa1b5
Anudip-Projects <aachal.mozare@anudip.org>
2026-03-28 12:39:18 +0530

    loop

---
+++ CoreJava_Basics/src/com/anudip/opearators/Test02_assign.java
@@ -0,0 +1,15 @@
+package com.anudip.opearators;
+
+public class Test02_assign {
+
+	public static void main(String[] args) {
+		
+		int age = 19;
+		
+		if(age>=18) {
+			System.out.println("person is elligible for voting");
+		}else {
+			System.out.println();
+		}
+	}
+}
